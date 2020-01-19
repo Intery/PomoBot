8b4a06f47cba3dd427ac9fdcd3b46c4e61823b90
Interitio <cona@thewisewolf.dev>
2020-01-19 17:48:39 +1000

    Update  type commands to use

--- bot/commands/exec.py
+++ bot/commands/exec.py
@@ -27,20 +27,17 @@ async def cmd_async(ctx):
     Description:
         Runs <code> as an asynchronous coroutine and prints the output or error.
     """
-    client = ctx.client
-    arg_str = ctx.arg_str
-    message = ctx.msg
-
-    if arg_str == "":
-        await message.channel.send("You must give me something to run!")
+    if ctx.arg_str == "":
+        await ctx.error_reply("You must give me something to run!")
         return
-    output, error = await _async(client, message, arg_str)
-    await message.channel.send("**Async input:**\
-                               \n```py\n{}\n```\
-                               \n**Output {}:** \
-                               \n```py\n{}\n```".format(arg_str,
-                                                        "error" if error else "",
-                                                        output))
+    output, error = await _async(ctx)
+    await ctx.reply(
+        "**Async input:**\
+        \n```py\n{}\n```\
+        \n**Output {}:** \
+        \n```py\n{}\n```".format(ctx.arg_str,
+                                 "error" if error else "",
+                                 output))
 
 
 @cmd("eval")
@@ -52,28 +49,24 @@ async def cmd_eval(ctx):
     Description:
         Runs <code> in current environment using eval() and prints the output or error.
     """
-    client = ctx.client
-    arg_str = ctx.arg_str
-    message = ctx.msg
-
-    if arg_str == "":
-        await message.channel.send("You must give me something to run!")
+    if ctx.arg_str == "":
+        await ctx.error_reply("You must give me something to run!")
         return
-    output, error = await _eval(client, message, arg_str)
-    await message.channel.send(
+    output, error = await _eval(ctx)
+    await ctx.reply(
         "**Eval input:**\
         \n```py\n{}\n```\
         \n**Output {}:** \
-        \n```py\n{}\n```".format(arg_str,
+        \n```py\n{}\n```".format(ctx.arg_str,
                                  "error" if error else "",
                                  output)
     )
 
 
-async def _eval(client, message, arg_str):
+async def _eval(ctx):
     output = None
     try:
-        output = eval(arg_str)
+        output = eval(ctx.arg_str)
     except Exception:
         return (str(traceback.format_exc()), 1)
     if asyncio.iscoroutine(output):
@@ -81,16 +74,19 @@ async def _eval(client, message, arg_str):
     return (output, 0)
 
 
-async def _async(client, message, arg_str):
-    env = {'client': client,
-           'message': message,
-           'arg_str': arg_str}
+async def _async(ctx):
+    env = {
+        'ctx': ctx,
+        'client': ctx.client,
+        'message': ctx.msg,
+        'arg_str': ctx.arg_str
+    }
     env.update(globals())
     old_stdout = sys.stdout
     redirected_output = sys.stdout = StringIO()
     result = None
     exec_string = "async def _temp_exec():\n"
-    exec_string += '\n'.join(' ' * 4 + line for line in arg_str.split('\n'))
+    exec_string += '\n'.join(' ' * 4 + line for line in ctx.arg_str.split('\n'))
     try:
         exec(exec_string, env)
         result = (redirected_output.getvalue(), 0)
