b03283d8c3bfb8ddf84d712fdb8b20e35e5d1c91
Interitio <cona@thewisewolf.dev>
2020-01-19 17:53:00 +1000

    core (Utilities): New seeker and interactive utilities.
    Seeker utilities `find_role` and `find_channel` added as Context utils.
    Interactive utilities `listen_for`, `selector` and `pager` added as
    Context utils.

---
+++ bot/utils/interactive.py
@@ -0,0 +1,226 @@
+import asyncio
+import discord
+from cmdClient import Context
+from cmdClient.lib import UserCancelled, ResponseTimedOut
+
+from .lib import paginate_list
+
+
+@Context.util
+async def listen_for(ctx, allowed_input=None, timeout=120, lower=True, check=None):
+    """
+    Listen for a one of a particular set of input strings,
+    sent in the current channel by `ctx.author`.
+    When found, return the message containing them.
+
+    Parameters
+    ----------
+    allowed_input: Union(List(str), None)
+        List of strings to listen for.
+        Allowed to be `None` precisely when a `check` function is also supplied.
+    timeout: int
+        Number of seconds to wait before timing out.
+    lower: bool
+        Whether to shift the allowed and message strings to lowercase before checking.
+    check: Function(message) -> bool
+        Alternative custom check function.
+
+    Returns: discord.Message
+        The message that was matched.
+
+    Raises
+    ------
+    cmdClient.lib.ResponseTimedOut:
+        Raised when no messages matching the given criteria are detected in `timeout` seconds.
+    """
+    # Generate the check if it hasn't been provided
+    if not check:
+        # Quick check the arguments are sane
+        if not allowed_input:
+            raise ValueError("allowed_input and check cannot both be None")
+
+        # Force a lower on the allowed inputs
+        allowed_input = [s.lower() for s in allowed_input]
+
+        # Create the check function
+        def check(message):
+            result = (message.author == ctx.author)
+            result = result and (message.channel == ctx.ch)
+            result = result and ((message.content.lower() if lower else message.content) in allowed_input)
+            return result
+
+    # Wait for a matching message, catch and transform the timeout
+    try:
+        message = await ctx.client.wait_for('message', check=check, timeout=timeout)
+    except asyncio.TimeoutError:
+        raise ResponseTimedOut("Session timed out waiting for user response.") from None
+
+    return message
+
+
+@Context.util
+async def selector(ctx, header, select_from, timeout=120, max_len=20):
+    """
+    Interactive routine to prompt the `ctx.author` to select an item from a list.
+    Returns the list index that was selected.
+
+    Parameters
+    ----------
+    header: str
+        String to put at the top of each page of selection options.
+        Intended to be information about the list the user is selecting from.
+    select_from: List(str)
+        The list of strings to select from.
+    timeout: int
+        The number of seconds to wait before throwing `ResponseTimedOut`.
+    max_len: int
+        The maximum number of items to display on each page.
+        Decrease this if the items are long, to avoid going over the char limit.
+
+    Returns
+    -------
+    int:
+        The index of the list entry selected by the user.
+
+    Raises
+    ------
+    cmdClient.lib.UserCancelled:
+        Raised if the user manually cancels the selection.
+    cmdClient.lib.ResponseTimedOut:
+        Raised if the user fails to respond to the selector within `timeout` seconds.
+    """
+    # Handle improper arguments
+    if len(select_from) == 0:
+        raise ValueError("Selection list passed to `selector` cannot be empty.")
+
+    # Generate the selector pages
+    footer = "Please type the number corresponding to your selection, or type `c` now to cancel."
+    list_pages = paginate_list(select_from, block_length=max_len)
+    pages = ["\n".join([header, page, footer]) for page in list_pages]
+
+    # Post the pages in a paged message
+    out_msg = await ctx.pager(pages)
+
+    # Listen for valid input
+    valid_input = [str(i+1) for i in range(0, len(select_from))] + ['c', 'C']
+    try:
+        result_msg = await ctx.listen_for(valid_input, timeout=timeout)
+    except ResponseTimedOut:
+        raise ResponseTimedOut("Selector timed out waiting for a response.")
+
+    # Try and delete the selector message and the user response.
+    try:
+        await out_msg.delete()
+        await result_msg.delete()
+    except discord.NotFound:
+        pass
+    except discord.Forbidden:
+        pass
+
+    # Handle user cancellation
+    if result_msg.content in ['c', 'C']:
+        raise UserCancelled("User cancelled selection.")
+
+    # The content must now be a valid index. Collect and return it.
+    index = int(result_msg.content) - 1
+    return index
+
+
+@Context.util
+async def pager(ctx, pages, locked=True, **kwargs):
+    """
+    Shows the user each page from the provided list `pages` one at a time,
+    providing reactions to page back and forth between pages.
+    This is done asynchronously, and returns after displaying the first page.
+
+    Parameters
+    ----------
+    pages: List(Union(str, discord.Embed))
+        A list of either strings or embeds to display as the pages.
+    locked: bool
+        Whether only the `ctx.author` should be able to use the paging reactions.
+    kwargs: ...
+        Remaining keyword arguments are transparently passed to the reply context method.
+
+    Returns: discord.Message
+        This is the output message, returned for easy deletion.
+    """
+    # Handle broken input
+    if len(pages) == 0:
+        raise ValueError("Pager cannot page with no pages!")
+
+    # Post first page. Method depends on whether the page is an embed or not.
+    if isinstance(pages[0], discord.Embed):
+        out_msg = await ctx.reply(embed=pages[0])
+    else:
+        out_msg = await ctx.reply(pages[0])
+
+    # Run the paging loop if required
+    if len(pages) > 1:
+        asyncio.ensure_future(_pager(ctx, out_msg, pages, locked))
+
+    # Return the output message
+    return out_msg
+
+
+async def _pager(ctx, out_msg, pages, locked):
+    """
+    Asynchronous initialiser and loop for the `pager` utility above.
+    """
+    # Page number
+    page = 0
+
+    # Add reactions to the output message
+    next_emoji = "▶"
+    prev_emoji = "◀"
+
+    try:
+        await out_msg.add_reaction(out_msg, prev_emoji)
+        await out_msg.add_reaction(out_msg, next_emoji)
+    except discord.Forbidden:
+        # We don't have permission to add paging emojis
+        # Die as gracefully as we can
+        await ctx.error_reply("Cannot page results because I do not have permissions to react!")
+        return
+
+    # Check function to determine whether a reaction is valid
+    def check(reaction, user):
+        result = reaction.message.id == out_msg.id
+        result = result and str(reaction.emoji) in [next_emoji, prev_emoji]
+        result = result and not (user.id == ctx.client.user.id)
+        result = result and not (locked and user != ctx.author)
+        return result
+
+    # Begin loop
+    while True:
+        # Wait for a valid reaction, break if we time out
+        try:
+            reaction, user = await ctx.bot.wait_for('reaction_add', check=check, timeout=300)
+        except asyncio.TimeoutError:
+            break
+
+        # Attempt to remove the user's reaction, silently ignore errors
+        asyncio.ensure_future(out_msg.remove_reaction(reaction.emoji, user))
+
+        # Change the page number
+        page += 1 if reaction.emoji == next_emoji else -1
+        page %= len(pages)
+
+        # Edit the message with the new page
+        active_page = pages[page]
+        if isinstance(active_page, discord.Embed):
+            await out_msg.edit(embed=active_page)
+        else:
+            await out_msg.edit(content=active_page)
+
+    # Clean up by removing the reactions
+    try:
+        await out_msg.clear_reactions()
+    except discord.Forbidden:
+        try:
+            await out_msg.remove_reaction(next_emoji, ctx.client.user)
+            await out_msg.remove_reaction(prev_emoji, ctx.client.user)
+        except discord.NotFound:
+            pass
+    except discord.NotFound:
+        pass
---
+++ bot/utils/seekers.py
@@ -0,0 +1,189 @@
+from cmdClient import Context
+from cmdClient.lib import InvalidContext, UserCancelled, ResponseTimedOut
+from . import interactive
+
+
+@Context.util
+async def find_role(ctx, userstr, interactive=False, collection=None):
+    """
+    Find a guild role given a partial matching string,
+    allowing custom role collections and several behavioural switches.
+
+    Parameters
+    ----------
+    userstr: str
+        String obtained from a user, expected to partially match a role in the collection.
+        The string will be tested against both the id and the name of the role.
+    interactive: bool
+        Whether to offer the user a list of roles to choose from,
+        or pick the first matching role.
+    collection: List(discord.Role)
+        Collection of roles to search amongst.
+        If none, uses the guild role list.
+
+    Returns
+    -------
+    discord.Role:
+        If a valid role is found.
+    None:
+        If no valid role has been found.
+
+    Raises
+    ------
+    cmdClient.lib.UserCancelled:
+        If the user cancels interactive role selection.
+    cmdClient.lib.ResponseTimedOut:
+        If the user fails to respond to interactive role selection within `60` seconds`
+    """
+    # Handle invalid situations and input
+    if not ctx.guild:
+        raise InvalidContext("Attempt to use find_role outside of a guild.")
+
+    if userstr == "":
+        raise ValueError("User string passed to find_role was empty.")
+
+    # Create the collection to search from args or guild roles
+    collection = collection if collection else ctx.guild.roles
+
+    # If the unser input was a number or possible role mention, get it out
+    roleid = userstr.strip('<#@&!>')
+    roleid = int(roleid) if roleid.isdigit() else None
+    searchstr = userstr.lower()
+
+    # Find the role
+    role = None
+
+    # Check method to determine whether a role matches
+    def check(role):
+        return (role.id == roleid) or (searchstr in role.name.lower())
+
+    # Get list of matching roles
+    roles = list(filter(check, collection))
+
+    if len(roles) == 0:
+        # Nope
+        role = None
+    elif len(roles) == 1:
+        # Select our lucky winner
+        role = roles[0]
+    else:
+        # We have multiple matching roles!
+        if interactive:
+            # Interactive prompt with the list of roles
+            role_names = [role.name for role in roles]
+
+            try:
+                selected = await ctx.selector(
+                    "`{}` roles found matching `{}`!".format(len(roles), userstr),
+                    role_names,
+                    timeout=60
+                )
+            except UserCancelled:
+                raise UserCancelled("User cancelled role selection.") from None
+            except ResponseTimedOut:
+                raise ResponseTimedOut("Role selection timed out.") from None
+
+            role = roles[selected]
+        else:
+            # Just select the first one
+            role = roles[0]
+
+    if role is None:
+        await ctx.error_reply("Couldn't find a role matching `{}`!")
+
+    return role
+
+
+@Context.util
+async def find_channel(ctx, userstr, interactive=False, collection=None, chan_type=None):
+    """
+    Find a guild channel given a partial matching string,
+    allowing custom channel collections and several behavioural switches.
+
+    Parameters
+    ----------
+    userstr: str
+        String obtained from a user, expected to partially match a channel in the collection.
+        The string will be tested against both the id and the name of the channel.
+    interactive: bool
+        Whether to offer the user a list of channels to choose from,
+        or pick the first matching channel.
+    collection: List(discord.Channel)
+        Collection of channels to search amongst.
+        If none, uses the full guild channel list.
+    chan_type: discord.ChannelType
+        Type of channel to restrict the collection to.
+
+    Returns
+    -------
+    discord.Channel:
+        If a valid channel is found.
+    None:
+        If no valid channel has been found.
+
+    Raises
+    ------
+    cmdClient.lib.UserCancelled:
+        If the user cancels interactive channel selection.
+    cmdClient.lib.ResponseTimedOut:
+        If the user fails to respond to interactive channel selection within `60` seconds`
+    """
+    # Handle invalid situations and input
+    if not ctx.guild:
+        raise InvalidContext("Attempt to use find_channel outside of a guild.")
+
+    if userstr == "":
+        raise ValueError("User string passed to find_channel was empty.")
+
+    # Create the collection to search from args or guild channels
+    collection = collection if collection else ctx.guild.roles
+    if chan_type is not None:
+        collection = [chan for chan in collection if chan.type == chan_type]
+
+    # If the user input was a number or possible channel mention, extract it
+    chanid = userstr.strip('<#@&!>')
+    chanid = int(chanid) if chanid.isdigit() else None
+    searchstr = userstr.lower()
+
+    # Find the channel
+    chan = None
+
+    # Check method to determine whether a channel matches
+    def check(chan):
+        return (chan.id == chanid) or (searchstr in chan.name.lower())
+
+    # Get list of matching roles
+    channels = list(filter(check, collection))
+
+    if len(channels) == 0:
+        # Nope
+        chan = None
+    elif len(channels) == 1:
+        # Select our lucky winner
+        chan = channels[0]
+    else:
+        # We have multiple matching channels!
+        if interactive:
+            # Interactive prompt with the list of channels
+            chan_names = [chan.name for chan in channels]
+
+            try:
+                selected = await ctx.selector(
+                    "`{}` channels found matching `{}`!".format(len(channels), userstr),
+                    chan_names,
+                    timeout=60
+                )
+            except UserCancelled:
+                raise UserCancelled("User cancelled channel selection.") from None
+            except ResponseTimedOut:
+                raise ResponseTimedOut("Channel selection timed out.") from None
+
+            chan = channels[selected]
+        else:
+            # Just select the first one
+            chan = channels[0]
+
+    if chan is None:
+        await ctx.error_reply("Couldn't find a channel matching `{}`!")
+
+    return chan
