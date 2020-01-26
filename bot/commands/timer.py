6bd2c50d473f6686c0e0ae87d605f1dc0d67d4c6
Interitio <cona@thewisewolf.dev>
2020-01-27 02:06:15 +1000

    (Timer commands): Rewrite and help for `join` command.
    Stubs for `groups` and `group` command.
    Rename `leave` command.

--- bot/commands/timer.py
+++ bot/commands/timer.py
@@ -1,33 +1,64 @@
 # import datetime
 # import discord
-
 from cmdClient import cmd
 
+from Timer import TimerState
 
-@cmd("sub")
-async def cmd_sub(ctx):
+from utils import timer
+
+
+@cmd("join",
+     group="Timer",
+     desc="Join a group bound to the current channel.",
+     aliases=['sub'])
+async def cmd_join(ctx):
+    """
+    Usage``:
+        join
+        join <group>
+    Description:
+        Join a group in the current channel.
+        If there are multiple matching groups, or no group is provided,
+        will show the group selector.
+    Examples``:
+        join espresso
+    """
+    # Quit if the author is already in a timer
     timer = ctx.client.interface.get_timer_for(ctx.author.id)
     if timer is not None:
         return await ctx.error_reply(
             "You are already in the group `{}` in {}!".format(timer.name, timer.channel.mention)
         )
 
-    tchan = ctx.client.interface.channels.get(ctx.ch.id, None)
-    if tchan is None or not tchan.timers:
-        return await ctx.error_reply("There are no timers in this channel!")
-    elif len(tchan.timers) == 1:
-        sub_to = tchan.timers[0]
+    # Get the timer they want to join
+    timer = await ctx.get_timers_matching(ctx.arg_str)
+
+    if timer is None:
+        return await ctx.error_reply(
+            ("No matching groups in this channel.\n"
+             "Use the `groups` command to see the groups in this guild!")
+        )
+
+    await ctx.client.interface.sub(ctx, ctx.author, timer)
+
+    message = "You have joined the group **{}**!".format(timer.name)
+    if timer.state == TimerState.RUNNING:
+        message += "\nCurrently on stage **{}** with **{}** remaining.".format(
+            timer.stages[timer.current_stage].name,
+            timer.pretty_remaining()
+        )
+    elif time.stages:
+        message += "\nGroup timer is set up but not running. Use `start` to start the timer!"
     else:
-        names = [timer.name for timer in tchan.timers]
-        sub_to = tchan.timers[await ctx.selector("Please select a group to join.", names)]
+        message += "\nSet up the timer with `set`!"
 
-    await ctx.client.interface.sub(ctx, ctx.author, sub_to)
-
-    await ctx.reply("You have joined **{}**!".format(sub_to.name))
-    # TODO: Summary of current status.
+    await ctx.reply(message)
 
 
-@cmd("unsub")
+@cmd("leave",
+     group="Timer",
+     desc="Leave your current group.",
+     aliases=['unsub'])
 async def cmd_unsub(ctx):
     timer = ctx.client.interface.get_timer_for(ctx.author.id)
     if timer is None:
@@ -80,3 +111,17 @@ async def cmd_start(ctx):
         return await ctx.error_reply("Please set up the timer first!")
 
     await timer.start()
+
+
+@cmd("groups",
+     group="Timer",
+     desc="View or manage the guild's groups.")
+async def cmd_groups(ctx):
+    pass
+
+
+@cmd("group",
+     group="Timer",
+     desc="View detailed information about a group.")
+async def cmd_group(ctx):
+    pass
