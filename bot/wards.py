63f122534a41a8c6cf280fd26c8816857bb93486
Interitio <cona@thewisewolf.dev>
2020-01-27 02:09:06 +1000

    (wards): New `timer_admin` ward.

---
+++ bot/wards.py
@@ -0,0 +1,18 @@
+from cmdClient import check
+
+
+@check(
+    name="TIMER_ADMIN",
+    msg=("You need to have one of the following to use this command.\n"
+         "- The `manage_guild` permission in this guild.\n"
+         "- The timer admin role (refer to the `adminrole` command).")
+)
+async def timer_admin(ctx, *args, **kwargs):
+    if ctx.author.guild_permissions.manage_guild:
+        return True
+
+    roleid = ctx.client.config.guilds.get(ctx.guild.id, "timeradmin")
+    if roleid is None:
+        return False
+
+    return roleid in [r.id for r in ctx.author.roles]
