b75885297b5aec3579b88916cbd17d0ef9b990a9
Interitio <cona@thewisewolf.dev>
2020-01-27 02:03:34 +1000

    fix (utils): Fixed typos in `embedreply` and changed default colour.

--- bot/utils/ctx_addons.py
+++ bot/utils/ctx_addons.py
@@ -1,13 +1,13 @@
 import discord
-from Context import Context
+from cmdClient import Context
 
 
 @Context.util
-async def embedreply(ctx, desc, colour=discord.Colour.red(), **kwargs):
+async def embedreply(ctx, desc, colour=discord.Colour(0x9b59b6), **kwargs):
     """
     Simple helper to embed replies.
     All arguments are passed to the embed constructor.
     `desc` is passed as the `description` kwarg.
     """
-    embed = discord.embed(description=desc, colour=colour, **kwargs)
+    embed = discord.Embed(description=desc, colour=colour, **kwargs)
     return await ctx.reply(embed=embed)
